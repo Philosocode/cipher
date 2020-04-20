6a01ed8b2cfb8d33acfb3458a0e09aba4ba3ce82
Tam Le <philosocode@gmail.com>
2020-04-20 14:58:00 -0600

    Remove delay on encode

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Cipher from "./XORCipher";
 import './App.css';
 
 const SECRET_KEY = "n2r3fn92rx92839x2398xe";
-
-const ENCODE_SECONDS = 0;
 const DECODE_SECONDS = 5;
 
 class App extends Component {
@@ -14,27 +12,15 @@ class App extends Component {
     result: "",
     currentTime: undefined,
     endTime: undefined,
-    timerRunning: true,
-    isEncoding: false
+    timerRunning: false,
   }
   
   /* Lifecycle */
   componentWillUnmount() {
-    this.stopTimer();
+    this.stopDecodeTimer();
   }
 
   /* Methods */
-  generateResult = () => {
-    const {content, isEncoding} = this.state;
-    if (!content) return;
-
-    const result = isEncoding
-      ? this.encode(content)
-      : this.decode(content);
-
-    this.setState({ result });
-  }
-
   decode = (content) => {
     return Cipher.decode(SECRET_KEY, content);
   }
@@ -43,26 +29,45 @@ class App extends Component {
     return Cipher.encode(SECRET_KEY, content);
   }
 
-  startTimer = (seconds, shouldEncode) => {
-    this.interval = setInterval(() => this.tick(), 1000);
-
-    const currentTime = new Date();
-    const endTime = new Date(currentTime.getTime() + (seconds * 1000));
-
-    this.setState({ timerRunning: true, isEncoding: shouldEncode, currentTime, endTime });
+  encodeContent = () => {
+    this.stopDecodeTimer();
+    this.setState({ endTime: new Date() });
+    this.generateContent(true);
   }
 
-  stopTimer() {
+  startDecodeTimer = () => {
+    this.stopDecodeTimer();
+
+    this.interval = setInterval(this.tick, 1000);
+
+    const currentTime = new Date();
+    const endTime = new Date(currentTime.getTime() + (DECODE_SECONDS * 1000));
+
+    this.setState({ timerRunning: true, currentTime, endTime });
+  }
+
+  stopDecodeTimer() {
     clearInterval(this.interval);
     this.setState({ timerRunning: false });
+  }
+
+  generateContent = (shouldEncode) => {
+    const { content } = this.state;
+    if (!content) return;
+
+    const result = shouldEncode
+      ? this.encode(content)
+      : this.decode(content);
+
+    this.setState({ result });
   }
 
   tick = () => {
     if (!this.state.timerRunning) return;
 
     if (this.getSecondsRemaining() <= 0) {
-      this.stopTimer();
-      this.generateResult();
+      this.stopDecodeTimer();
+      this.generateContent(false);
     } else {
       this.setState({ currentTime: new Date() });
     }
@@ -119,12 +124,8 @@ class App extends Component {
         />
   
         <div className="btn__container">
-          <button className="btn btn--encode" onClick={() => this.startTimer(ENCODE_SECONDS, true)}>
-            Encode
-          </button>
-          <button className="btn btn--decode" onClick={() => this.startTimer(DECODE_SECONDS, false)}>
-            Decode
-          </button>
+          <button className="btn btn--encode" onClick={this.encodeContent}>Encode</button>
+          <button className="btn btn--decode" onClick={this.startDecodeTimer}>Decode</button>
         </div>
 
         { this.displayResult() }
